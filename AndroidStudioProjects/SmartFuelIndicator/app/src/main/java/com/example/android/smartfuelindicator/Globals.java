84d611bfa60f7a35278cb3a8b7ea166c2fcaef9c
superSaiyan3 <prajyot.suvarna10@gmail.com>
2017-04-16 22:39:21 +0530

    Update Globals.java

--- AndroidStudioProjects/SmartFuelIndicator/app/src/main/java/com/example/android/smartfuelindicator/Globals.java
+++ AndroidStudioProjects/SmartFuelIndicator/app/src/main/java/com/example/android/smartfuelindicator/Globals.java
@@ -5,4 +5,43 @@ package com.example.android.smartfuelindicator;
  */
 
 public class Globals {
+    private static Globals instance;
+    private static float fuelValue;
+    public static String model;
+    public static double mileage;
+
+    private Globals(){
+
+    }
+
+    public void setMileage(double value) {
+        Globals.mileage=value;
+    }
+
+    public double getMileage() {
+        return Globals.mileage;
+    }
+
+    public void setModel(String anotherString) {
+        Globals.model=anotherString;
+    }
+
+    public String getModel() {
+        return Globals.model;
+    }
+
+    public void setFuelValue(float value) {
+        Globals.fuelValue = value;
+    }
+
+    public float getFuelValue() {
+        return Globals.fuelValue;
+    }
+
+    public static synchronized Globals getInstance() {
+        if(instance == null) {
+            instance = new Globals();
+        }
+        return instance;
+    }
 }
