4dbd9bf82070dd1ed4600c0351b941dd1cccb883
superSaiyan3 <prajyot.suvarna10@gmail.com>
2017-04-16 22:42:31 +0530

    Update MapsActivity.java

--- AndroidStudioProjects/SmartFuelIndicator/app/src/main/java/com/example/android/smartfuelindicator/MapsActivity.java
+++ AndroidStudioProjects/SmartFuelIndicator/app/src/main/java/com/example/android/smartfuelindicator/MapsActivity.java
@@ -1,18 +1,41 @@
 package com.example.android.smartfuelindicator;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.example.android.smartfuelindicator.R.id.map;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    Globals g = Globals.getInstance();
+    float fuelReading;
+    double carMileage,totalDist;
     private GoogleMap mMap;
+    static final int REQUEST_LOCATION = 1;
+    int year;
+    TextView txtTotalDist;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +43,67 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
         mapFragment.getMapAsync(this);
+
+        txtTotalDist = (TextView) findViewById(R.id.tvMileage);
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+                    g.setMileage(jsonResponse.getDouble("mileage"));
+                    year = jsonResponse.getInt("yom");
+                    if(success) {
+                        txtTotalDist.setText("Distance to zero:"+Math.round(g.getFuelValue()*g.getMileage())+"km");
+                        fuelReading=g.getFuelValue();
+                        carMileage = g.getMileage();
+                        totalDist = fuelReading*carMileage*1000;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        MileageRequest mileageRequest = new MileageRequest(g.getModel(),responseListener);
+        RequestQueue queue = Volley.newRequestQueue(MapsActivity.this);
+        queue.add(mileageRequest);
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+        else {
+            // Add a marker in Bangalore and move the camera
+            LatLng bangalore = new LatLng(12.8615, 77.6647);
+            mMap.addMarker(new MarkerOptions().position(bangalore).title("Vehicle Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bangalore, 8));
+            mMap.setMyLocationEnabled(true);
+            CircleOptions circleOptions = new CircleOptions()
+                    .center(new LatLng(12.8615, 77.6647))
+                    .radius(1000) // In meters
+                    .strokeColor(Color.RED)
+                    .fillColor(Color.LTGRAY);
+
+            // Get back the mutable Circle
+            Circle circle = mMap.addCircle(circleOptions);
+
+
+
+        }
     }
 }
+
